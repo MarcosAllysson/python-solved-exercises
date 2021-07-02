343767c85eb9b3477bb7c07e22e3e4a535f5c646
MarcosAllysson <marcosgracip05@gmail.com>
2021-07-02 20:49:35 -0300

    program 9

--- 3 - Estrutura Listas/q9.py
+++ 3 - Estrutura Listas/q9.py
@@ -1,3 +1,19 @@
+from functools import reduce
+# from random import randint
+"""
+Faça um Programa que leia um vetor A com 10 números inteiros, calcule e mostre a soma dos quadrados dos elementos do 
+vetor.
 """
 
-"""
+try:
+    vetores_inteiros = []
+    for i in range(10):
+        vetores_inteiros.append(int(input(f'{i+1}° vetor: ')))
+        # vetores_inteiros.append(int(randint(1, 100)))
+
+    vetor_ao_quadrado = list(map(lambda x: x ** 2, vetores_inteiros))
+    soma_vetor_ao_quadrado = reduce(lambda a, b: a + b, vetor_ao_quadrado)
+    print(f'Soma dos quadrados: {soma_vetor_ao_quadrado}')
+
+except Exception as erro:
+    print(erro)
