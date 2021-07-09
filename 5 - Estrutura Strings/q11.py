ab302e84f3dbb81a5d20d44dde96d316ddb189c1
MarcosAllysson <marcosgracip05@gmail.com>
2021-07-09 13:42:40 -0300

    jogo da forca

--- 5 - Estrutura Strings/q11.py
+++ 5 - Estrutura Strings/q11.py
@@ -15,20 +15,86 @@ Digite uma letra: S
 -> Você errou pela 2ª vez. Tente de novo!
 """
 
+from random import choice
+from time import sleep
 
-while True:
-    palavra = 'JESUS'
-    palavra_temp = list()
+word_list = [
+    'churrasco',
+    'ferradura',
+    'sorriso',
+    'faqueiro',
+    'periquito',
+    'baralho',
+    'tartaruga',
+    'grande',
+    'brincadeira',
+    'passagem',
+    'vassoura',
+    'cheirosa',
+    'avestruz',
+    'xadrez',
+    'guerreiro',
+    'formigueiro',
+    'guitarra',
+    'caranguejo',
+    'avental',
+    'impressora',
+    'chiclete',
+    'bicicleta',
+    'inglesa',
+    'surpresa'
+]
 
-    letra = str(input('Digite uma letra: ')).strip().upper()[0]
-
-    if letra in palavra:
-        palavra_temp.append(letra)
-
-    else:
-        print('Errou!')
-
-    print(palavra_temp)
+# Timer sleep 3 segundos
+print('SORTEANDO PALAVRA...')
+sleep(3)
+word = choice(word_list)
 
 
+palavra_split = list()
+letras_acertadas_usuario = list()
+cont = 0
 
+# Palavra secreta do jogo:
+for letra in word:
+    palavra_split.append(letra.upper())
+
+# Preenchendo lista com traços:
+for i in range(0, len(palavra_split)):
+    letras_acertadas_usuario.append('_')
+
+try:
+    acertou = False
+
+    while not acertou:
+        print()  # quebrando linha
+        letra = str(input('\nDigite uma letra: ')).strip().upper()[0]
+
+        # Validando se é letra:
+        if letra.isalpha():
+            print(f'A palavra é: ', end=' ')
+
+            for i in range(0, len(palavra_split)):
+                if letra == palavra_split[i]:
+                    letras_acertadas_usuario[i] = letra
+
+                print(letras_acertadas_usuario[i], end=' ')
+
+            acertou = True
+
+            for x in range(0, len(letras_acertadas_usuario)):
+                if letras_acertadas_usuario[x] == '_':
+                    acertou = False
+
+        # Diferente de letra, jogada inválida.
+        else:
+            print('Jogada inválida. Jogue somente letra.')
+
+except IndexError:
+    print('Espaços em branco não são permitidos.')
+
+except TypeError:
+    print('Jogada não permitida, tipo permitido: letra')
+
+except Exception as error:
+    print(f'Erro: {error.__class__}.')
