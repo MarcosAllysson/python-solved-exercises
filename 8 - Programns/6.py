56f33156ecd414c84a6fb37ea8a21d4c11fe54e5
MarcosAllysson <marcosgracip05@gmail.com>
2021-08-18 09:34:43 -0300

    Exercise 6: Remove empty strings from the list of strings

--- 8 - Programns/6.py
+++ 8 - Programns/6.py
@@ -5,3 +5,17 @@ Give:
 Expected output:
     ["Mike", "Emma", "Kelly", "Brad"]
 """
+
+
+def remove_empty_string(list_string):
+    new_string = []
+
+    for item in list_string:
+        if not item == '':
+            new_string.append(item)
+
+    return new_string
+
+
+list1 = ["Mike", "", "Emma", "Kelly", "", "Brad"]
+print(remove_empty_string(list1))
