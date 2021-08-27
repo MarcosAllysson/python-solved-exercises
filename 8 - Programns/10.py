f8c38ac99a6f56fb2f3c6117db74c9d6f7744c4f
MarcosAllysson <marcosgracip05@gmail.com>
2021-08-27 08:43:27 -0300

    Exercise 10: Given a Python list, remove all occurrence of 20 from the list

--- 8 - Programns/10.py
+++ 8 - Programns/10.py
@@ -5,3 +5,17 @@ Given:
 Expected output:
     [5, 15, 25, 50]
 """
+
+
+def remove_occurrence(original_list, item):
+
+    if len(original_list) > 0:
+        for i in original_list:
+            if i == item:
+                original_list.remove(i)
+
+    return original_list
+
+
+list1 = [5, 20, 15, 20, 25, 50, 20]
+print(remove_occurrence(list1, 20))
